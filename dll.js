e7a4347ec2317250d91e370cbef259c15c8c19ba
Upshur06 <mupshur2005@gmail.com>
2021-05-03 01:22:50 -0400

    prepend/append_nodes

--- dll.js
+++ dll.js
@@ -17,22 +17,62 @@ export class Node {
      * Push a new value to the tail of the linked list.
      */
   
-    push(value) {}
+    push(value) {
+        let node = new Node(value);
+
+        if(!this.head){
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node
+        }
+        this.length++;
+    }
   
     /**
      * Remove an item from the end of the linked list.
      */
-    pop() {}
+    pop() {
+        if(!this.tail){
+            return null;
+        } else {
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+        }
+        this.length--;
+    }
   
     /**
      * Remove a node from the beginning of the list.
      */
-    shift() {}
+    shift() {
+        if(!this.head){
+            return null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+        }
+        this.length--;
+    }
   
     /**
      * Add a node to the head of the linked list.
      */
-    unshift(value) {}
+    unshift(value) {
+        let node = new Node(value);
+
+        if(!this.head){
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.head.prev = node;
+            node.next = this.head;
+            this.head = node;
+        }
+        this.length++;
+    }
   
     /**
      * Get a Node at a specific index
